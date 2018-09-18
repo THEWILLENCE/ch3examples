86bf6d795e8dafe4dc52ce7ab577172bf070907f
william.ence <william.ence@CLC-LAB209-16.TCSD.tooelesd.org>
2018-09-18 14:02:48 -0600

    HEHE

--- ch3exe/src/WillEnce/ch3exe/ComputeArea.java
+++ ch3exe/src/WillEnce/ch3exe/ComputeArea.java
@@ -12,20 +12,22 @@ public class ComputeArea {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		//constant declinations
+		final double PI = 3.14159265359793284626;
+		
 		//step 1: enter the radius of a circle
-		double PI = 3.14159265359793284626;
-		
-		
 		double radius; 
 		double area;
+		
 		
 		//assign a value to radius
 		System.out.println("enter the radius of a circle and press enter: ");
 		radius = input.nextDouble(); 
-		
+		System.out.println(radius);
 		
 		//step 2: compute the area using the following formula
-		//area - radius*radius*pi
+		//area = radius*radius*pi
 		
 		area = radius*radius*PI;
 		
---
+++ ch3exe/src/WillEnce/ch3exe/VariableExe.java
@@ -0,0 +1,56 @@
+package WillEnce.ch3exe;
+
+public class VariableExe {
+
+	public static void main(String[] args) {
+		//datatype variableName;
+
+		//letters
+		char char1;//a single character 8 bits
+		//true or false
+		boolean bool1;//true or false 64 bits
+		//whole numbers
+		byte x1;// (-128 to 127) 8bits
+		nextByte()
+		short x2; // -32768 to 32767 16 bits
+		nextShort()
+		int num1;//whole numbers +or- (-2147483648 to 214748367) 32 bits\
+		nextLong()
+		long x3; //(-9223372036854775808 to 9223372036854775807) 64 bits
+		
+		//real numbers
+		double num2;//fractional number +or- 64 bits -1.7976931348623157E +308 to -409e-324
+		nextDouble()
+		float x4; // Negative range: -3.4028235E + 38 to -1.4E - 45 32 bits
+		nextFloat()
+		String x5; // 64 bits not not a data type
+		next()
+		nextline()
+		
+		num1 = 10;
+		num2 = 3.754;
+		char1 = 'f';
+		bool1 = true;
+		
+		num1 = num1 + num1;
+		num1 = (6*5)*9*(5+4);
+		int x,y,j,k;
+		x = y = j = k = 5;
+		
+		int z = 50;
+		System.out.println(x=7);
+		 
+		int count;
+		count = 1;
+		
+		
+		
+		x=count++;
+		System.out.print(x);
+		x=++count;
+		System.out.print(x);
+		
+		
+	}
+
+}
